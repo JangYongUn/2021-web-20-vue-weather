af1a19bfee24449e33f1ec969c764b6dec5311c0
JangYongUn <sm970413@naver.com>
2021-02-19 13:20:41 +0900

    ....

---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+	.search-wrapper
+		b-form-select(v-model="selected" :options="options" size="lg")
+</template>
+<script>
+export default {
+	name: 'Search',
+	data() {
+		return {
+			selected: null,
+			city: [
+				{ value: '1811'}
+			]
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+	@import '@/assets/scss/_mixins';
+	.search-wrapper {
+
+	}
+</style>
