e173f3fc5510ed36b62836628edd8944023bd717
JangYongUn <sm970413@naver.com>
2021-02-19 12:49:42 +0900

    ......

---
+++ src/router/dist/index.dev.js
@@ -0,0 +1,41 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _vueRouter = _interopRequireDefault(require("vue-router"));
+
+var _Home = _interopRequireDefault(require("../views/Home.vue"));
+
+var _Daily = _interopRequireDefault(require("../views/Daily.vue"));
+
+var _Days = _interopRequireDefault(require("../views/Days.vue"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].use(_vueRouter["default"]);
+
+var routes = [{
+  path: '/',
+  name: 'Home',
+  component: _Home["default"]
+}, {
+  path: '/daily',
+  name: 'Daily',
+  component: _Daily["default"]
+}, {
+  path: '/5days',
+  name: 'Days',
+  component: _Days["default"]
+}];
+var router = new _vueRouter["default"]({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
+});
+var _default = router;
+exports["default"] = _default;
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Home from '../views/Daily.vue'
-import Home from '../views/Days.vue'
+import Daily from '../views/Daily.vue'
+import Days from '../views/Days.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/daily',
-    name: 'Daily',
-    component: Daily
-  },
-  {
-    path: '/5days',
-    name: 'Days',
-    component: Days
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/daily',
+		name: 'Daily',
+		component: Daily
+	},
+	{
+		path: '/5days',
+		name: 'Days',
+		component: Days
+	},
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
 
 export default router
