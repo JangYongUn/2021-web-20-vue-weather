a6c20deb8dcad2455185f6698063557990cbbd54
JangYongUn <sm970413@naver.com>
2021-02-23 11:32:46 +0900

    .....

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,2 +1,12 @@
+// 현재 위치의 navigator.getPosition()
+const getCoords = () => {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition((r) => {
+			resolve({ lat: r.coords.latitude, lon: r.coords.longitude })
+		}, (err) => {
+			reject(err)
+		})
+	});
+}
 
-export 
+export { getCoords }
